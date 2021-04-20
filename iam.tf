2327e672782f205dd71e911b6ae880e9d4a6b7bd
faisalsalhi <faisal.salhi@etu.univ-smb.fr>
2021-04-20 05:32:30 +0200

    Modified boundaries and created file for resource policies of ostrees, modified iam.tf to apply the changes

--- iam.tf
+++ iam.tf
@@ -25,25 +25,7 @@ resource "aws_iam_policy" "allow-ostree-master-dir-fullcontrol" {
   path = "/"
   description = "A policy to allow Read and Write operation into the Ostree master in the SaaSProj Bucket"
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-        {
-          "Effect": "Allow",
-          "Action": [
-            "s3:PutObject",
-            "s3:GetObject",
-            "s3:ListBucket",
-            "s3:DeleteObject",
-          ],
-          "Resource": [
-            "arn:aws:s3:::saasproj/ostree/master",
-            "arn:aws:s3:::saasproj/ostree/master/*"
-          ]
-        }
-    ]
-  })
-
+  policy = jsonencode(file("iam_policy/rw-ostree-master.json"))
 }
 
 resource "aws_iam_policy" "allow-ostree-worker-dir-fullcontrol" {
@@ -51,236 +33,23 @@ resource "aws_iam_policy" "allow-ostree-worker-dir-fullcontrol" {
   path = "/"
   description = "A policy to allow Read and Write operation into the Ostree worker in the SaaSProj Bucket"
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-        {
-          "Effect": "Allow",
-          "Action": [
-            "s3:PutObject",
-            "s3:GetObject",
-            "s3:ListBucket",
-            "s3:DeleteObject",
-          ],
-          "Resource": [
-            "arn:aws:s3:::saasproj/ostree/worker",
-            "arn:aws:s3:::saasproj/ostree/worker/*"
-          ]
-        }
-    ]
-  })
+  policy = jsonencode(file("iam_policy/rw-ostree-worker.json")
 }
 
 
   # TODO : Create the IAM Role for manipulating S3/SaaSproj/ostree
     # TODO : Create the IAM Instance Profile for manipulating S3/SaaSProj/ostree
 
-############################ S3 Bucket Dir. Ostree Policies ############################
-
-  # TODO : Create the IAM Role for reading into these folders
-    # TODO : Create the IAM Instance Profile for reading these
-
-resource "aws_iam_policy" "allow-ostree-master-dir-read" {
-  name = "Allow-Ostree-Master-Dir-Read"
+resource "aws_iam_policy" "admin_saas_boundaries" {
+  name = "Admin-SaaS-Boundaries"
   path = "/"
-  description = "A policy to allow Read and Write operation into the Ostree worker in the SaaSProj Bucket"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-        {
-          "Effect": "Allow",
-          "Action": [
-            "s3:GetObject",
-            "s3:ListBucket",
-          ],
-          "Resource": [
-            "arn:aws:s3:::SaaSProj/ostree/master",
-            "arn:aws:s3:::SaaSProj/ostree/master/*"
-          ]
-        }
-    ]
-  })
+  description = "The boundaries for the admnistrator_saas group"
+  policy = jsonencode(file("iam_policy/admin-saas-boundaries.json"))
 }
 
-resource "aws_iam_policy" "allow-ostree-worker-dir-read" {
-  name = "Allow-Ostree-Worker-Dir-Read"
+resource "aws_iam_policy" "instance_saas_boundaries" {
+  name = "Instance-SaaS-Boundaries"
   path = "/"
-  description = "A policy to allow Read and Write operation into the Ostree worker in the SaaSProj Bucket"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-        {
-          "Effect": "Allow",
-          "Action": [
-            "s3:GetObject",
-            "s3:ListBucket",
-          ],
-          "Resource": [
-            "arn:aws:s3:::SaaSProj/ostree/worker",
-            "arn:aws:s3:::SaaSProj/ostree/worker/*"
-          ]
-        }
-    ]
-  })
+  description = "The boundaries for the instances created by adminstrator_saas group"
+  policy = jsonencode(file("iam_policy/instance-saas-boundaries.json"))
 }
-
-
-  # TODO : Create the IAM Role for reading into these
-    # TODO : Create the IAM Instance Profile for reading into these
-
-############################ S3 Bucket Instances Assets Policies ######################
-
-# TODO : Create the IAM Policy to manipulate the S3 Folders with { Project : "SaaS" ; Type : "instance-assets" } S3/SaaSProj/instances bucket dir
-  # TODO : Assign the policy to Administrator_SaaS
-
-
-resource "aws_iam_policy" "allow-ostree-instances-dir-fullcontrol" {
-  name = "Allow-Ostree-Worker-Dir-Read"
-  path = "/"
-  description = "A policy to allow Read and Write operation into the Ostree worker in the SaaSProj Bucket"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-        {
-          "Effect": "Allow",
-          "Action": [
-            "s3:PutObject",
-            "s3:GetObject",
-            "s3:ListBucket",
-            "s3:DeleteObject"
-          ],
-          "Resource": [
-            "arn:aws:s3:::SaaSProj/instances",
-            "arn:aws:s3:::SaaSProj/instances/*"
-          ]
-        }
-    ]
-  })
-}
-
-
-# TODO : Create the IAM Policy to create IAM Policies for S3 Folders with { Project : "SaaS" ; Type : "instance-assets" & isPresentKey => instanceName }
-
-
-resource "aws_iam_policy" "saas-admin-iam-rights" {
-  name = "saas-admin-iam-rights"
-  path = "/"
-  description = "A policy to allow Read and Write operation into the Ostree worker in the SaaSProj Bucket"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-        {
-          "Sid" : "IAMTaskAllowed"
-          "Effect": "Allow",
-          "Action": [
-            "iam:CreatePolicy",
-            "iam:CreateRole",
-            "iam:AddRoleToInstanceProfile",
-            "iam:RemoveRoleFromInstanceProfile",
-            "iam:AttachRolePolicy",
-            "iam:CreateInstanceProfile",
-            "iam:DeleteInstanceProfile",
-            "iam:DeletePolicy",
-            "iam:DeleteRole",
-            "iam:DetachRolePolicy",
-            "iam:GetInstanceProfile",
-            "iam:GetPolicy",
-            "iam:GetRole",
-            "iam:TagInstanceProfile",
-            "iam:TagPolicy",
-            "iam:TagRole",
-            "iam:UntagInstanceProfile",
-            "iam:UntagPolicy",
-            "iam:UntagRole",
-            "iam:UpdateRole",
-            "iam:UpdateRoleDescription",
-            "iam:UpdateAssumeRolePolicy",
-            "iam:PassRole"
-          ],
-          "Resource": [
-          ]
-        },
-        {
-          "Sid" : "DenyPermBoundaryIAMPolicyAlteration",
-          "Effect": "Deny",
-          "Action": [
-            "iam:DeletePolicy",
-            "iam:DeletePolicyVersion",
-            "iam:CreatePolicyVersion",
-            "iam:SetDefaultPolicyVersion"
-          ],
-          "Resource": [
-            "arn:aws:iam::#TODO : AccountID:policy/saas-admin-iam-rights"
-          ]
-        },
-        {
-          "Sid" : "DenyRemovalOfPermBoundaryFromAnyUserOrRole",
-          "Effect" : "Deny",
-          "Action" : [
-              "iam:DeleteUserPermissionsBoundary",
-              "iam:DeleteRolePermissionsBoundary"
-          ],
-          "Resource": [
-              "arn:aws:iam::#TODO : AccountID:user/*",
-              "arn:aws:iam::#TODO : AccountID:role/*",
-          ],
-          "Condition": {
-              "StringEquals" : {
-                "iam:PermissionsBoundary": "arn:aws:iam::#TODO : AcccountID:policy/saas-admin-iam-rights"
-              }
-          }
-        },
-        {
-          "Sid" : "DenyUserAndRoleCreationWithOutPermBoundary",
-          "Effect": "Deny",
-          "Action": [
-            "iam:CreateUser",
-            "iam:CreateRole",
-          ],
-          "Resource": [
-            "arn:aws:iam::#TODO : AccountID:user/*",
-            "arn:aws:iam::#TODO : AccountID:role/*"
-          ],
-          "Condition": {
-            "StringNotEquals": "arn:aws:iam::#TODO : AccountID:policy/saas-admin-iam-rights"
-          }
-        }
-    ]
-  })
-}
-
-############################ SaaS Administrator Rights ################################
-
-# TODO : Code the IAM policy for i3 Metal Instance (PowerOn / PowerOff instance)
-  # TODO : Assign the IAM Policy for using the i3 Metal to Administrator_Saas group
-
-# TODO : Create the policy allowing the creation/destruction/manipulation of instances 
-  # TODO : Assign to group Administrator_SaaS
-
-# TODO : Create the policy allowing the creation/destruction/manipulation of VPCs
-  # TODO : Assign to group Administrator_SaaS
-
-# TODO : Create the policy allowing the creation/destruction/manipulation of Subnets
-  # TODO : Assign to group Administrator_SaaS
-
-# TODO : Create the policy allowing the creation/destruction/manipulation of NAT
-  # TODO : Assign to group Administrator_SaaS
-
-# TODO : Create the policy allowing the creation/destruction/manipulation of Security Groups
-  # TODO : Assign to group Administrator_SaaS
-
-# TODO : Create the policy allowing the creation/destruction/manipulation of Routing Table
-  # TODO : Assign to group Administrator_SaaS
-
-# TODO : Create the policy allowing the creation/destruction/manipulation of Internet Gateway
-  # TODO : Assign to group Administrator_SaaS
-
-# TODO : Create the policy allowing the manipulation of the S3/TerraformState/SaaS Bucket
-  # TODO : Assign to group Administrator_SaaS
-
-# TODO : Create the policy allowing the right to assign instance profiles (worker/master) to instances
-  # TODO : Assign to group Administrator_SaaS
