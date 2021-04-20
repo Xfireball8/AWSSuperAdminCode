fcc286a88c2dc8bf658d1a755890dccccc307adc
faisalsalhi <faisal.salhi@etu.univ-smb.fr>
2021-04-20 06:20:15 +0200

    Removed call to jsonencode function in iam.tf as it is error prone and not necessary with the use of a json file

--- iam.tf
+++ iam.tf
@@ -21,7 +21,7 @@ resource "aws_iam_policy" "allow-ostree-master-dir-fullcontrol" {
   path = "/"
   description = "A policy to allow Read and Write operation into the Ostree master in the SaaSProj Bucket"
 
-  policy = jsonencode(file("iam_policy/rw-ostree-master.json"))
+  policy = file("iam_policy/rw-ostree-master.json")
 }
 
 resource "aws_iam_policy" "allow-ostree-worker-dir-fullcontrol" {
@@ -29,30 +29,26 @@ resource "aws_iam_policy" "allow-ostree-worker-dir-fullcontrol" {
   path = "/"
   description = "A policy to allow Read and Write operation into the Ostree worker in the SaaSProj Bucket"
 
-  policy = jsonencode(file("iam_policy/rw-ostree-worker.json"))
+  policy = file("iam_policy/rw-ostree-worker.json")
 }
 
 resource "aws_iam_group_policy" "allow-poweron-shutdown-i3metal" {
   name = "Allow-Poweron-Shutdown-i3metal"
   group = aws_iam_group.administrator-saas.name
-  path = "/"
-  description = "A policy to allow PowerOn/Reboot/Shutdown on i3 Workstations"
 
-  policy = jsonencode(file("iam_policy/poweron-shutdown-i3metal.json"))
+  policy = file("iam_policy/poweron-shutdown-i3metal.json")
 }
 
 
 resource "aws_iam_group_policy" "admin_saas_boundaries" {
   name = "Admin-SaaS-Boundaries"
   group = aws_iam_group.administrator-saas.name
-  path = "/"
-  description = "The boundaries for the admnistrator_saas group"
-  policy = jsonencode(file("iam_policy/admin-saas-boundaries.json"))
+  policy = file("iam_policy/admin-saas-boundaries.json")
 }
 
 resource "aws_iam_policy" "instance_saas_boundaries" {
   name = "Instance-SaaS-Boundaries"
   path = "/"
   description = "The boundaries for the instances created by adminstrator_saas group"
-  policy = jsonencode(file("iam_policy/instance-saas-boundaries.json"))
+  policy = file("iam_policy/instance-saas-boundaries.json")
 }
