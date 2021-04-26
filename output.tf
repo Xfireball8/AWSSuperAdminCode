3e29097f2a9879cb4edc0a24bc625f3a821f0691
faisalsalhi <faisal.salhi@etu.univ-smb.fr>
2021-04-27 01:54:51 +0200

    Remove output var public ip address of the workstation

--- output.tf
+++ output.tf
@@ -1,10 +1,4 @@
 # TODO : Create output variables for instance variable ssh login
-
-output "workstation_ip" {
-  description = "Public IP of the workstation, Connect through ssh with user : ubuntu" 
-  value = aws_instance.i3metal_workstation.public_ip 
-}
-
 output "password" {
   value = aws_iam_user_login_profile.sfaisal-saas_login_profile.encrypted_password
 }
